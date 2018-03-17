752524662e06ec80f5c4c7d8c14fdaedb28a542a
Mike Rainey <bionic.oldsmobile@gmail.com>
2018-03-17 23:00:44 +0100

    adding option to build documentation

--- script/default.nix
+++ script/default.nix
@@ -1,21 +1,35 @@
-{ stdenv, fetchurl }:
+{ pkgs   ? import <nixpkgs> {},
+  stdenv ? pkgs.stdenv,
+  fetchurl,
+  buildDocs ? false
+}:
 
 stdenv.mkDerivation rec {
   name = "cmdline-${version}";
-  version = "1.0";
+  version = "1.1";
 
   src = fetchurl {
     url = "https://github.com/deepsea-inria/cmdline/archive/${version}.tar.gz";
     sha256 = "d947aa8fec769e417684a924e3f5bf76beaacda9c51725d9f955047c98e4ab39";
   };
 
-  dontBuild = true;
+  buildInputs = if buildDocs then [
+    pkgs.pandoc pkgs.texlive.combined.scheme-medium
+  ] else
+    [];
+
+  buildPhase = if buildDocs then ''
+    make README.pdf README.html
+  ''
+  else ''
+    # nothing to build
+  '';
 
   installPhase = ''
     mkdir -p $out/include/
     cp include/cmdline.hpp $out/include/
     mkdir -p $out/doc
-    cp README.md Makefile cmdline.css $out/doc/
+    cp README.* Makefile cmdline.css $out/doc/
   '';
 
   meta = {
